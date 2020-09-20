25ad9180a5c6cdece5f8c09573d2362913d7dac8
mohini-gupta <mohini.gupta@mail.polimi.it>
2020-09-20 16:36:03 +0530

    added permission for camera

--- app/src/main/java/com/example/streamvideo/MainActivity.java
+++ app/src/main/java/com/example/streamvideo/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.streamvideo;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,14 +17,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
